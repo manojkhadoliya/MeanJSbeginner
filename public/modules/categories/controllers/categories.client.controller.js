474557a5d8c721998bd837473ecd61f35eda222f
Manoj Saini <manoj.saini@pearson.com>
2016-11-23 18:52:47 +0530

    added category service

--- public/modules/categories/controllers/categories.client.controller.js
+++ public/modules/categories/controllers/categories.client.controller.js
@@ -1,8 +1,25 @@
 'use strict';
 
-angular.module('categories').controller('CategoriesController', ['$scope',
-	function($scope) {
-		// Controller Logic
-		// ...
+angular.module('categories').controller('CategoriesController', ['$scope', '$location', 'Categories',
+
+	function($scope, $location, Categories) {
+		//create a category
+		$scope.create = function() {
+			var category = new Categories({
+				name: this.name,
+				description: this.description
+			});
+			category.$save(function(response) {
+				$location.path('categories/' + response._id);
+				$scope.name = '';
+			}, function(errResponse) {
+				$scope.error = errResponse.data.message;
+			});
+
+
+		};
+		$scope.find = function() {
+			$scope.categories = Categories.query();
+		};
 	}
 ]);
---
+++ public/modules/categories/services/categories.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('categories').factory('Categories', ['$resource',
+
+	function($resource) {
+		return $resource('categories/:categoryId', {
+			categoryId: '@_id'
+		}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+
+]);
