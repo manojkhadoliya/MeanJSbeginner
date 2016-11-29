71aba238a9487675a65b1e9f70b93141dd0be246
Manoj Saini <manoj.saini@pearson.com>
2016-11-29 13:34:35 +0530

    added update and remove a category

--- public/modules/categories/controllers/categories.client.controller.js
+++ public/modules/categories/controllers/categories.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('categories').controller('CategoriesController', ['$scope', '$stateParams','$location', 'Categories',
+angular.module('categories').controller('CategoriesController', ['$scope', '$stateParams', '$location', 'Categories',
 
 	function($scope, $stateParams, $location, Categories) {
 		//create a category
@@ -22,9 +22,31 @@ angular.module('categories').controller('CategoriesController', ['$scope', '$sta
 			$scope.categories = Categories.query();
 		};
 		$scope.findOne = function() {
-			$scope.category = Categories.get({ 
+			$scope.category = Categories.get({
 				categoryId: $stateParams.categoryId
 			});
+		};
+		$scope.update = function() {
+			var category = $scope.category;
+			category.$update(function() {
+				$location.path('categories/' + category._id);
+			}, function(errResponse) {
+				$scope.error = errResponse.data.message;
+			});
+		};
+		$scope.remove = function(category) {
+			if (category) {
+				category.$remove();
+				for (var i in $scope.categories) {
+					if ($scope.categories[i] == category) {
+						$scope.categories.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.category.$remove(function() {
+					$location.path('categories');
+				});
+			}
 		};
 	}
 ]);
