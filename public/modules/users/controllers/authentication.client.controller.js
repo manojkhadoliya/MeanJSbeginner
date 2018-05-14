21c1a9024428b8d5a29ae8e59b3b92e6f21c7705
Manoj Saini <manoj.saini@comprotechnologies.com>
2018-05-14 15:27:54 +0530

    updted controller

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,5 +1,30 @@
 'use strict';
+angular.module('users').directive('validPasswordC', function() {
+	return {
+		require: 'ngModel',
+		scope: {
+			reference : 'validPasswordC'
+		},
+		controller: 'AuthenticationController',
 
+		link: function(scope, elm, attrs, ctrl) {
+			
+			ctrl.$parsers.unshift(function(viewValue, $scope) {
+
+				var noMatch = viewValue !== scope.reference;
+				console.log('viewValue',viewValue);
+				console.log('scope.reference',scope.reference);
+				ctrl.$setValidity('noMatch', !noMatch);
+				scope.passMatch = !noMatch;
+				return (noMatch) ? noMatch : !noMatch;
+			});
+
+			scope.$watch('reference', function(value) {
+				ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
+			});
+		}
+	};
+});
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
 		$scope.authentication = Authentication;
@@ -8,6 +33,8 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		if ($scope.authentication.user) $location.path('/');
 
 		$scope.signup = function() {
+			console.log('**********',$scope.passMatch);
+
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
